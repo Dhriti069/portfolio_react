fb3a45198106c24a800eb91e2a1c89e704fe4bad
Dhriti069 <98838127+Dhriti069@users.noreply.github.com>
2023-07-20 01:04:16 +0530

    xyz

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import RESUME from '../../assets/RESUME.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={RESUME} download className='btn'>Download Resume</a>
+      <a href="https://drive.google.com/file/d/1wyLyc2L6w-Ake9r9dvMGEUFOb1wgoM5w/view?usp=sharing" download className='btn'>Download Resume</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
